0b5a9d8e6e2e181fd16acd1a636e6eebffd4af5d
Jon Evans <jon@craftyjon.com>
2013-11-19 19:24:57 -0500

    Remove skips from some scrapers

--- scrapers/harvard_mosc_events.py
+++ scrapers/harvard_mosc_events.py
@@ -2,5 +2,4 @@ from lib.scraper import ICalScraper
 
 
 class HarvardMoSCEventsScraper(ICalScraper):
-    skip = True
     scrape_url = "http://hmsc.harvard.edu/calendar/export.ics"
--- scrapers/hms_events.py
+++ scrapers/hms_events.py
@@ -3,6 +3,5 @@ from lib.scraper import ICalScraper
 
 class HMSEventsScraper(ICalScraper):
     #This will need heavy filtering to not be annoying
-    skip = True
     scrape_url = "http://www.trumba.com/calendars/harvardmedicalschool.ics"
     additional_fields = ['LOCATION', ['URL', 'event_url']]
